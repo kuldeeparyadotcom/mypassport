2f70fcab6996e7aa71ecb618957b89f4fe761980
KD <kuldeeparyadotcom@gmail.com>
2016-07-16 16:16:50 -0500

    working code after refactoring

--- mypassport/app.js
+++ mypassport/app.js
@@ -5,18 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-//Adding for Facebook login using passport middleware
-var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
 
 var app = express();
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
 // Configure view engine to render EJS templates.
 app.set('views', __dirname + '/views');
@@ -30,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+*/
 
 app.use('/', routes);
 app.use('/users', users);
@@ -40,56 +34,11 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-*/
-
-//Adding for facebook login
-passport.use(new Strategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    //User your domain or local development server address.. devopsmonk.com, 192.168.55.56:3000
-    //The same address must be provided in Valid OAuth redirect URIs (Under Facebook login over https://developers.facebook.com/apps/)
-    callbackURL: 'http://192.168.55.56:3000/login/facebook/return'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
-  }));
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
-
-  passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
 
 
+
+
+/*
 // Define routes.
 app.get('/',
   function(req, res) {
@@ -115,7 +64,7 @@ app.get('/profile',
   function(req, res){
     res.render('profile', { user: req.user });
   });
-
+*/
 
 /*
 // error handlers
--- mypassport/routes/index.js
+++ mypassport/routes/index.js
@@ -1,9 +1,77 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Passport Authentication Service' });
+
+//Adding for facebook login
+passport.use(new Strategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    //User your domain or local development server address.. devopsmonk.com, 192.168.55.56:3000
+    //The same address must be provided in Valid OAuth redirect URIs (Under Facebook login over https://developers.facebook.com/apps/)
+    callbackURL: 'http://192.168.55.56:3000/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
 });
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+router.use(require('morgan')('combined'));
+router.use(require('cookie-parser')());
+router.use(require('body-parser').urlencoded({ extended: true }));
+router.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get('/', function(req, res, next) {
+  res.render('home', { user: req.user });
+});
+
+router.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+router.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+router.get('/login/facebook/return', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+router.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
 
 module.exports = router;
